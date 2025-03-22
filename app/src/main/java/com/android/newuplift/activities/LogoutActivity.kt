79c5118b4a920b5e87daa0506f67b075ae74defe
Christain Aranez Nemeno <christiannemeno80@gmail.com>
2025-03-22 17:37:58 +0800

    todo logout

--- app/src/main/java/com/android/newuplift/activities/LogoutActivity.kt
+++ app/src/main/java/com/android/newuplift/activities/LogoutActivity.kt
@@ -3,11 +3,8 @@ package com.android.newuplift.activities
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.android.newuplift.R
 
 class LogoutActivity : AppCompatActivity() {
