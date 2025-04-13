a43d50589399e29568f8f0beea189782af56be8c
Christain Aranez Nemeno <christiannemeno80@gmail.com>
2025-04-13 08:09:50 +0800

    my quotes added, for more info there are comments explaining each functions
    things i added
    
    makeQUote Fragment
    the xml of this
    
    in main activity added navigation
    
    in quote dao added a function to query for user made quotes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,9 @@ dependencies {
 
     implementation("com.google.android.material:material:1.12.0")
 
+    implementation ("com.google.android.material:material:1.7.0")
+    implementation ("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+
     //
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
